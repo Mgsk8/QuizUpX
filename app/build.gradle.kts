92b0e9078f31a7b441ec889bcb76752dc2746b5d
Miguel Escobar <maem1704@gmail.com>
2025-06-23 23:49:35 -0500

    Cambios de Bedoya

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation ("androidx.room:room-runtime:2.6.1")
     annotationProcessor ("androidx.room:room-compiler:2.6.1")
+    implementation ("com.google.android.material:material:1.11.0")
 
     implementation ("androidx.room:room-ktx:2.6.1")
 
